d13b9057923b712e187bcd11f5ca3413dffd13ac
tower2149 <tower2149.aic@gmail.com>
2024-05-23 15:45:55 +0900

    端末から画面の諸元を取得

--- src/main.rs
+++ src/main.rs
@@ -3,35 +3,34 @@ use std::process;
 use viuer::{print_from_file, Config};
 
 
-fn get_terminal_size(terminal_w: &mut usize, terminal_h: &mut usize) {
+fn get_terminal_size(terminal_col: &mut usize, terminal_lines: &mut usize, terminal_pix_x: &mut usize, terminal_pix_y: &mut usize) {
 
     use crossterm::terminal::*;
     enable_raw_mode();
-    match xterm_query::query("\x1b[14t", 10000 as u64) {
-        Ok(content) => println!("terminal size pixel: {:?}", content),
-        Err(err) => eprintln!("Failed to read terminal size: {}", err),
-    }
+    let Ok(response) = xterm_query::query("\x1b[14t", 100 as u64) else { process::exit(0x0100); };
     disable_raw_mode();
+    let terminal_size_pixel = response.replace("t", ";");
+    let terminal_size_pixel = terminal_size_pixel.split(';').collect::<Vec<&str>>().clone();
+    *terminal_pix_x = terminal_size_pixel[1].to_string().parse().unwrap();
+    *terminal_pix_y = terminal_size_pixel[2].to_string().parse().unwrap();
 
     // 結果を表示
     if let Some((w, h)) = term_size::dimensions() {
-        println!("Width: {}\nHeight: {}", w, h);
-        *terminal_w = w;
-        *terminal_h = h;
+        *terminal_col = w;
+        *terminal_lines = h;
     } else {
-        println!("Unable to get term size :(");
         process::exit(0x0100);
     }
 }
 
-fn showimg(terminal_w: &usize, terminal_h: &usize) {
+fn showimg(terminal_col: &usize, terminal_lines: &usize) {
     let conf = Config {
         // set offset
         x: 0,
         y: 0,
         // set dimensions
-        width: Some(*terminal_w as u32),
-        height: Some(*terminal_h as u32),
+        width: Some(*terminal_col as u32),
+        height: Some(*terminal_lines as u32),
         ..Default::default()
     };
 
@@ -39,8 +38,19 @@ fn showimg(terminal_w: &usize, terminal_h: &usize) {
 }
 
 fn main() {
-    let mut terminal_w : usize = 0;
-    let mut terminal_h : usize = 0;
-    get_terminal_size(&mut terminal_w, &mut terminal_h);
-    // showimg(&terminal_w, &terminal_h);
+    let mut terminal_col : usize = 0;
+    let mut terminal_lines : usize = 0;
+    let mut terminal_pix_x : usize = 0;
+    let mut terminal_pix_y : usize = 0;
+    get_terminal_size(
+        &mut terminal_col,
+        &mut terminal_lines,
+        &mut terminal_pix_x,
+        &mut terminal_pix_y
+        );
+    println!("{}", &terminal_col);
+    println!("{}", &terminal_lines);
+    println!("{}", &terminal_pix_x);
+    println!("{}", &terminal_pix_y);
+    // showimg(&terminal_col, &terminal_lines);
 }
